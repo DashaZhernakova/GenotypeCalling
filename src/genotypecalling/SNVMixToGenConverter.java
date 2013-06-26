27921bce62e2113f0a5d6ef94d0acc0f0ec74647
DashaZhernakova <dashazhernakova@gmail.com>
2013-06-26 10:44:50 +0200

    Added support for file input as a list

--- src/genotypecalling/SNVMixToGenConverter.java
+++ src/genotypecalling/SNVMixToGenConverter.java
@@ -19,6 +19,8 @@ import umcg.genetica.io.trityper.util.ChrAnnotation;
  * @author dashazhernakova
  */
 public class SNVMixToGenConverter {
+    String[] files;
+    String[] samples;
     private HashMap<String, Integer> individualMap;
     private ArrayList<String> individuals;
     private HashMap<String, Integer> snpMap;
@@ -60,7 +62,7 @@ public class SNVMixToGenConverter {
                 throw new IOException("Error: could not find dir: " + dir);
             }
 
-            String[] files = getFnames(dir, snvmixFnamePattern);
+            getFnames(dir, snvmixFnamePattern);
 
             System.out.println("Found " + files.length + " SNVMix genotype files");
             if (files.length == 0) {
@@ -73,211 +75,277 @@ public class SNVMixToGenConverter {
                 System.out.println(f);
 
             
-            makeMatrix(files);
+            makeMatrix();
 
             writeGenotypesByChr(outPrefix, conversionVCFFile);
         }
+        
+    public void parse(String fileListPath, String outPrefix, String conversionVCFFile) throws IOException {
+        getFnames(fileListPath);
+
+        System.out.println("\nFile with samples and file paths: " + fileListPath);
+        System.out.println("Output prefix: " + outPrefix);
+
+        System.out.println("Found " + files.length + " SNVMix genotype files");
+        if (files.length == 0) {
+                System.exit(0);
+        }
+
+        numInds = files.length;
+
+        for (String f : files)
+            System.out.println(f);
+
+        makeMatrix();
+
+        writeGenotypesByChr(outPrefix, conversionVCFFile);
+    }
         /**
-	 * Gets files whose names match snvmixFnamePattern (regex)
+	 * Gets files from folder dirName whose names match snvmixFnamePattern (regex)
 	 * 
-	 * @param dir
+	 * @param dirName
 	 * @param snvmixFnamePattern regexp pattern for matching genotype files
 	 * 
 	 */
-        private String[] getFnames(String dirName, String snvmixFnamePattern){
+     private void getFnames(String dirName, String snvmixFnamePattern){
         File dir = new File(dirName);
         ArrayList<String> fnames = new ArrayList<String>();
+        ArrayList<String> sampleNames = new ArrayList<String>();
         for (File ch : dir.listFiles())
             if (ch.isDirectory())
                 for (File child : ch.listFiles())
-                   if ((child.getName().matches(snvmixFnamePattern)) && (child.length() != 0))
+                   if ((child.getName().matches(snvmixFnamePattern)) && (child.length() != 0)){
                        fnames.add(child.getPath());
+                       sampleNames.add(dirName);
+                   }
                 
-        return fnames.toArray(new String[0]);           
-        }
-        
-        private void getAllSNPs(String[] files) throws IOException{
-            
-            //get a set of all SNPs
-            HashSet<String> allSNPs = new HashSet<String>();
-            for (String f : files){
-                TextFile tf = new TextFile(f, TextFile.R);
-                String [] elems;
-                
-                while ((elems = tf.readLineElems(TextFile.tab)) != null){
-                    if (elems[0].contains("chr"))
-                        elems[0] = elems[0].replaceFirst("chr", "");
-                    int gen = Integer.parseInt(elems[3].split(",")[5]);
-                    float probability = Float.parseFloat(elems[3].split(",")[1 + gen]);
-                    
-                    if (probability >= p_threshold)
-                        allSNPs.add(elems[0]);
-                }
-                tf.close();
-            }
-            
-            //make SNP map
-            String[] uniqueSNPsArray = allSNPs.toArray(new String[0]);
-            allSNPs.clear();
-            snpMap = new HashMap<String, Integer>();
-            snpChrMap = new HashMap<String, Byte>();
-            snpChrPosMap = new HashMap<String, Integer>();
-            snpSet = new TreeSet<String>(COMPARE_BY_POS);
-            //chromosomes = new 
-            int snpCtr = 0;
-            for (String snp : uniqueSNPsArray){
-                Byte chr = ChrAnnotation.parseChr(snp.split(":")[0]);
-		Integer pos = Integer.parseInt(snp.split(":")[1]);
-                
-                //chromosomes.add(snp.split(":")[0]);
-                snpSet.add(snp);
-                snpMap.put(snp, snpCtr);
-                snpChrPosMap.put(snp, pos);
-                snpChrMap.put(snp, chr);
-                snpCtr++;
-            }
-            numSNPs = snpSet.size();
-        }
-        
-        private void makeMatrix(String[] files) throws IOException{
-            
-            getAllSNPs(files);
-            
-            matrix = new float[numSNPs][numInds][3];
-            
-            individuals = new ArrayList<String>();
-            individualMap = new HashMap<String, Integer>();
-            alleles = new HashMap<Integer, String>();
-            
-            SNVMix snvmix = new SNVMix();
-            int indCnt = 0;
-            
-            for (String sampleFile : files){
-                TextFile sFile = new TextFile(sampleFile, TextFile.R);
-                
-                String[] els;
-                int gen;
-                float[] prob;
-                String ref, alt;
-                
-                while ( (els = sFile.readLineElems(TextFile.tab)) != null ){
-                    
-                    if (els[0].contains("chr"))
-                        els[0] = els[0].replaceFirst("chr", "");
-                    
-                    ref = els[1];
-                    alt = els[2];
-                    
-                    gen = Integer.parseInt(els[3].split(",")[5]);
-                    prob = new float[] {Float.parseFloat(els[3].split(",")[2]), Float.parseFloat(els[3].split(",")[3]), Float.parseFloat(els[3].split(",")[4])};;
-                    
-                    if (prob[gen - 1] >= p_threshold){
-                        Integer snpId = snpMap.get(els[0]);
-                        
-                        alleles.put(snpId, ref + alt);
-                        
-                        matrix [snpId][indCnt][0] = prob[0];
-                        matrix [snpId][indCnt][1] = prob[1];
-                        matrix [snpId][indCnt][2] = prob[2];
+        files = fnames.toArray(new String[0]);
+        samples = sampleNames.toArray(new String[0]);
+     }
+
+    /**
+     * Gets paths to files if a list of samples and corresponding file paths is given
+     * @param fileList - path to the file with sample names in the first column and paths to snvmix files in the second column (tab as a separator)
+     * @throws IOException
+     */
+    private void getFnames(String fileList) throws IOException {
+        TextFile flist = new TextFile(fileList, false);
+        files = flist.readAsArray(1, TextFile.tab);
+        samples = new String[files.length];
+        flist.close();
+        flist = new TextFile(fileList, false);
+        samples = flist.readAsArray(0, TextFile.tab);
+        flist.close();
+    }
+
+    /**
+     * Gets all SNPs that pass the p-value threshold and are diallelic
+     * @throws IOException
+     */
+    private void getAllSNPs() throws IOException{
+
+        //get a map of all SNPs mapped to their alleles
+        HashMap<String, String> allSNPs = new HashMap<String, String>();
+        HashSet<String> triallelic = new HashSet<String>();
+        for (String f : files){
+            TextFile tf = new TextFile(f, TextFile.R);
+            String [] els;
+            int gen;
+            float probability;
+            String alleles;
+            while ((els = tf.readLineElems(TextFile.tab)) != null){
+                if (els[0].contains("chr"))
+                    els[0] = els[0].replaceFirst("chr", "");
+                gen = Integer.parseInt(els[3].split(",")[5]);
+                probability = Float.parseFloat(els[3].split(",")[1 + gen]);
+                alleles = els[1] + els[2];
+                if (probability >= p_threshold) {
+                    if (triallelic.contains(els[0])) {
+                        continue;
                     }
-                    
-                 }
-                
-                sFile.close();
-                
-                String sampleId = getSampleNameFromFname(sampleFile);
-                individuals.add(sampleId);
-                individualMap.put(sampleId, indCnt);
-                indCnt++;
+                    if (allSNPs.containsKey(els[0])){
+                        if (!allSNPs.get(els[0]).equals(alleles)) {//alleles not equal to previous instance of that SNP => not diallelic
+                            allSNPs.remove(els[0]);
+                            triallelic.add(els[0]);
+                        }
+                    }
+                    else
+                        allSNPs.put(els[0], alleles);
+                }
             }
-            
+            tf.close();
         }
-        
-        private void writeGenotypes (String outPrefix, String conversionVCFFile) throws IOException {
-            
-            System.out.println("\nTotal number of detected SNPs: " + snpSet.size());
-            System.out.println("Writing SNPs to output file...");
-            
-            TextFile gen = new TextFile(outPrefix + ".gen", true);
-            
-            System.out.println("\nWriting genotypes...");
-                        
-            if (conversionVCFFile != null){ //convert to rs ids
-                System.out.println("Will be using " + conversionVCFFile + " to convert SNP ids");
-                makeSNPIdConverter(conversionVCFFile);
-            }
-            
-            for (String snp : snpSet){
+
+
+        //make SNP map and position maps
+        String[] uniqueSNPsArray = allSNPs.keySet().toArray(new String[0]);
+        allSNPs.clear();
+
+        snpMap = new HashMap<String, Integer>();
+        snpChrMap = new HashMap<String, Byte>();
+        snpChrPosMap = new HashMap<String, Integer>();
+        snpSet = new TreeSet<String>(COMPARE_BY_POS);
+        int snpCtr = 0;
+        for (String snp : uniqueSNPsArray){
+            Byte chr = ChrAnnotation.parseChr(snp.split(":")[0]);
+            Integer pos = Integer.parseInt(snp.split(":")[1]);
+
+            snpSet.add(snp);
+            snpMap.put(snp, snpCtr);
+            snpChrPosMap.put(snp, pos);
+            snpChrMap.put(snp, chr);
+            snpCtr++;
+        }
+        numSNPs = snpSet.size();
+    }
+
+    /**
+     * Creates a genotype matrix containing genotype probabilities: matrix[SNPid][sampleId][type] (type: 0 - homozygote by ref, 1 - heterozygote, 2 - homozygote by alt)
+     * @throws IOException
+     */
+    private void makeMatrix() throws IOException{
+        //get all diallelic SNPs passing p-value threshold
+        getAllSNPs();
+
+        matrix = new float[numSNPs][numInds][3];
+        individuals = new ArrayList<String>();
+        individualMap = new HashMap<String, Integer>();
+        alleles = new HashMap<Integer, String>();
+
+        int indCnt = 0;
+
+        for (String sampleFile : files){
+            TextFile sFile = new TextFile(sampleFile, TextFile.R);
+
+            String[] els;
+            int gen;
+            float[] prob;
+            String ref, alt;
+
+            while ( (els = sFile.readLineElems(TextFile.tab)) != null ){
+
+                if (els[0].contains("chr"))
+                    els[0] = els[0].replaceFirst("chr", "");
+
+                ref = els[1];
+                alt = els[2];
+                gen = Integer.parseInt(els[3].split(",")[5]);
+                prob = new float[] {Float.parseFloat(els[3].split(",")[2]), Float.parseFloat(els[3].split(",")[3]), Float.parseFloat(els[3].split(",")[4])};;
+
+                if (prob[gen - 1] >= p_threshold){
+                    if (! snpSet.contains(els[0])) //skip if triallelic
+                        continue;
+
+                    Integer snpId = snpMap.get(els[0]);
+                    alleles.put(snpId, ref + alt);
+
+                    matrix [snpId][indCnt][0] = prob[0];
+                    matrix [snpId][indCnt][1] = prob[1];
+                    matrix [snpId][indCnt][2] = prob[2];
+                }
+
+             }
+
+            sFile.close();
+
+            String sampleId = samples[indCnt];
+            individuals.add(sampleId);
+            individualMap.put(sampleId, indCnt);
+            indCnt++;
+        }
+
+    }
+
+    private void writeGenotypes (String outPrefix, String conversionVCFFile) throws IOException {
+
+        System.out.println("\nTotal number of detected SNPs: " + snpSet.size());
+        System.out.println("Writing SNPs to output file...");
+
+        TextFile gen = new TextFile(outPrefix + ".gen", true);
+
+        System.out.println("\nWriting genotypes...");
+
+        if (conversionVCFFile != null){ //convert to rs ids
+            System.out.println("Will be using " + conversionVCFFile + " to convert SNP ids");
+            makeSNPIdConverter(conversionVCFFile);
+        }
+
+        for (String snp : snpSet){
+            gen.writeln(printGenotypes(snp));
+        }
+
+        gen.close();
+
+        System.out.println("\nTotal number of detected individuals: " + individuals.size());
+        System.out.println("Writing individuals to output file...");
+
+        TextFile sample = new TextFile(outPrefix + ".sample", true);
+        sample.writeln("ID_1 ID_2 missing");
+        sample.writeln("0 0 0");
+        for (String ind : individuals) {
+                sample.writeln(ind + " " + ind + " 0");
+        }
+
+        sample.close();
+        System.out.println("\nFinished.");
+    }
+
+    private void writeGenotypesByChr (String outPrefix, String conversionVCFFile) throws IOException {
+
+        //Write samples
+        System.out.println("\nTotal number of detected individuals: " + individuals.size());
+        System.out.println("Writing individuals to output file...");
+
+        TextFile sample = new TextFile(outPrefix + ".sample", true);
+        sample.writeln("ID_1 ID_2 missing");
+        sample.writeln("0 0 0");
+        for (String ind : individuals) {
+                sample.writeln(ind + " " + ind + " 0");
+        }
+        sample.close();
+
+
+        //Write genotypes
+        System.out.println("\nTotal number of detected SNPs: " + snpSet.size());
+        System.out.println("Writing SNPs to output file...");
+        System.out.println("\nWriting genotypes by chromosomes...");
+
+        if (conversionVCFFile != null){ //convert to rs ids
+            System.out.println("Will be using " + conversionVCFFile + " to convert SNP ids");
+            makeSNPIdConverter(conversionVCFFile);
+        }
+        else
+            System.out.println("Will not convert SNP ids");
+
+        String curChr = "", chr;
+        int nSNPs = 0;
+        TextFile gen = null;
+        for (String snp : snpSet){
+            chr = snp.split(":")[0];
+            if (curChr.equals(chr)){
                 gen.writeln(printGenotypes(snp));
+                nSNPs++;
             }
-            
-            gen.close();   
-            
-            System.out.println("\nTotal number of detected individuals: " + individuals.size());
-            System.out.println("Writing individuals to output file...");
-            
-            TextFile sample = new TextFile(outPrefix + ".sample", true);
-            sample.writeln("ID_1 ID_2 missing");
-            sample.writeln("0 0 0");
-            for (String ind : individuals) {
-                    sample.writeln(ind + " " + ind + " 0");
-            }
-                        
-            sample.close();
-            System.out.println("\nFinished.");
-        }
-        
-        private void writeGenotypesByChr (String outPrefix, String conversionVCFFile) throws IOException {
-            
-            System.out.println("\nTotal number of detected individuals: " + individuals.size());
-            System.out.println("Writing individuals to output file...");
-            
-            TextFile sample = new TextFile(outPrefix + ".sample", true);
-            sample.writeln("ID_1 ID_2 missing");
-            sample.writeln("0 0 0");
-            for (String ind : individuals) {
-                    sample.writeln(ind + " " + ind + " 0");
-            }
-                        
-            sample.close();
-            
-            
-            System.out.println("\nTotal number of detected SNPs: " + snpSet.size());
-            System.out.println("Writing SNPs to output file...");
-            
-                      
-            System.out.println("\nWriting genotypes by chomosomes...");
-                        
-            if (conversionVCFFile != null){ //convert to rs ids
-                System.out.println("Will be using " + conversionVCFFile + " to convert SNP ids");
-                makeSNPIdConverter(conversionVCFFile);
-            }
-            
-            String curChr = "", chr;
-            TextFile gen = null;
-            for (String snp : snpSet){
-                chr = snp.split(":")[0];
-                if (curChr.equals(chr)){
-                    gen.writeln(printGenotypes(snp));
+            else{
+                if (! curChr.isEmpty()){
+                    gen.close();
+                    System.out.println(" (" + nSNPs + " SNPs)");
                 }
-                else{
-                    if (! curChr.isEmpty()){
-                        gen.close();
-                    }
-                    
-                    //next chr
-                    curChr = chr;
-                    System.out.println("Writing genotypes on chr" + curChr);
-                    gen = new TextFile(outPrefix + "_" + curChr + ".gen", true);
-                    gen.writeln(printGenotypes(snp));
-                }
-                
+                //System.out.println(" (" + nSNPs + " SNPs)");
+                nSNPs = 0;
+                //next chr
+                curChr = chr;
+                System.out.print("Writing genotypes on chr" + curChr);
+                gen = new TextFile(outPrefix + "_" + curChr + ".gen", true);
+                gen.writeln(printGenotypes(snp));
+                nSNPs++;
             }
-            
-            gen.close();   
-            
-            System.out.println("\nFinished.");
         }
+        gen.close();
+        System.out.println(" (" + nSNPs + " SNPs)");
+        System.out.println("\nFinished.");
+    }
+
     /*
      * Makes a HashMap to use when converting snvmix position ids (chr:pos) to rs ids
      * conversionFile - path to the VCF file
@@ -338,10 +406,13 @@ public class SNVMixToGenConverter {
     
     public static void main(String[] args) throws IOException {
         SNVMixToGenConverter c = new SNVMixToGenConverter();
-        c.parse("/Users/dashazhernakova/Documents/UMCG/data/geuvadis/mappedData_masked", 
+        /*c.parse("/Users/dashazhernakova/Documents/UMCG/data/geuvadis/mappedData_masked",
             "/Users/dashazhernakova/Documents/UMCG/data/geuvadis/genotypes/test", 
             "reads_unique_hits.*cov5.filtered.snvmix", 
-            null);
+            null); */
+        c.parse("/Users/dashazhernakova/Documents/UMCG/GeneticalGenomicsDatasets/tmp.txt",
+                "/Users/dashazhernakova/Documents/UMCG/data/geuvadis/genotypes/test",
+                null);
     }
 }            
 
